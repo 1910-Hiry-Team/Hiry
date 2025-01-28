c219c76f2f1c8eebc8797fdbbb2af5281285b714
Joachim Clodic <joachim.clodic@gmail.com>
2025-01-28 11:21:58 +0100

    generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'jobs/index'
+  get 'jobs/show'
+  get 'jobs/new'
+  get 'jobs/create'
+  get 'jobs/edit'
+  get 'jobs/update'
+  get 'jobs/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
