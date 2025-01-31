eec4bd491bca023476d1434cd82b1cdf812cb784
Nicolas Beernaert <nicobeernaert@hotmail.com>
2025-01-31 16:08:15 +0100

    added the create application in a modal (#15)
    Co-authored-by: Alessian Dormal <alessian@alessian.be>

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -10,6 +10,26 @@ class ApplicationsController < ApplicationController
     @company = current_user.company
   end
 
+  def new
+    @application = Application.new
+  end
+
   def edit
+  end
+
+  def create
+    @job = Job.find(params[:job_id])
+    @application = current_user.applications.new(application_params)
+    if @application.save
+      redirect_to @job, notice: 'Application was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def application_params
+    params.require(:application).permit(:stage, :match, :job_id)
   end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,6 +14,7 @@ class JobsController < ApplicationController
 
   def show
     @favorite = Favorite.new
+    @application = Application.new
   end
 
   def new
