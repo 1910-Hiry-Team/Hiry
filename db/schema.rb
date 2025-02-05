48f4ca9fab30f194f4a8bd1952755b4f2978389d
Maxime Nogues <maxime.nogues1408@gmail.com>
2025-02-05 18:03:15 +0100

    Front end for landing page (#24)
    * Done
    
    * Setup the font Poppins, added the color chart, almost done with the landing page, I'm trying to display a video in the background
    
    * Changing the Le wagon logo to hiry logo
    
    * The layout of the landing page is good, I'm waiting the TA to be able to display the video as a background
    
    * The video in the back in now working
    
    * Getting on the style for the forms
    
    * The form for jobseeker is almost done styling
    
    * Front end landing page, forms done
    
    * Logout page front end done
    
    * The front end is done for landing page, sign up jobseeker & company, log out, log in
    
    * Final push
    
    * Final push with bigger title on landing page
    
    * Final push with the modification the role data type from string to integer
    
    ---------
    
    Co-authored-by: Nicolas Beernaert <nicobeernaert@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_03_104224) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_05_165409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
