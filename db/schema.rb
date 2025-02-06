f88987ee4899e4db6ca28358a72b19607391f5af
maxime nogues <maxime.nogues1408@gmail.com>
2025-02-06 11:23:08 +0100

    Fixing heroku bugs

--- db/schema.rb
+++ db/schema.rb
@@ -86,6 +86,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_06_085248) do
     t.string "hobbies"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
     t.index ["user_id"], name: "index_jobseeker_profiles_on_user_id"
   end
 
