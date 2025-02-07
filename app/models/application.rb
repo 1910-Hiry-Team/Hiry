023f5cdc93583fb737ae159aa45d3b874c2f83c1
Maxime Nogues <maxime.nogues1408@gmail.com>
2025-02-07 09:07:18 +0100

    Recovering the applications mistake (#38)

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,6 +3,9 @@ class Application < ApplicationRecord
   belongs_to :user
   belongs_to :job
 
+  has_one :company, through: :jobs
+
+
   # Validations
   validates :stage, inclusion: { in: ["Applied", "Interviewing", "Hired", "Rejected"] }
   validates :match, inclusion: { in: [true, false] }
